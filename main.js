3acecc3b5dd607071607198776b2ef1113da3d90
Park-Beomjin <87348209+pbj97@users.noreply.github.com>
2022-06-22 14:40:41 +0900

    JS: Home transparant

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
 'use strict';
 
-
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
 });
 
 // Handle scrolling when trapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null){
-        return;
-    }
-    scrollIntoView(link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
 });
-
 
 // Handle click on "contact me" button on home
 const contactBtn = document.querySelector('.home__contact');
-contactBtn.addEventListener('click' , (event) => {
-    scrollIntoView('#contact')
+contactBtn.addEventListener('click', (event) => {
+  scrollIntoView('#contact');
+});
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 function scrollIntoView(selector) {
-    const scrollto = document.querySelector(selector);
-    scrollto.scrollIntoView({behavior: "smooth"});
+  const scrollto = document.querySelector(selector);
+  scrollto.scrollIntoView({ behavior: 'smooth' });
 }
