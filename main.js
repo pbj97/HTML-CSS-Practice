218ecf77efc21acb86fc50b03e599bb1f42afe43
Park-Beomjin <87348209+pbj97@users.noreply.github.com>
2022-06-22 16:21:40 +0900

    JS: project filtering

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ navbarMenu.addEventListener('click', (event) => {
 
 // Handle click on "contact me" button on home
 const contactBtn = document.querySelector('.home__contact');
-contactBtn.addEventListener('click', (event) => {
+contactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
@@ -43,8 +43,32 @@ document.addEventListener('scroll', () => {
     arrowUp.classList.remove('visible');
   }
 });
+
+// Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
+});
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
 });
 
 function scrollIntoView(selector) {
