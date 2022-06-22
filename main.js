5d622c2d89b7d82c74ea1dc0339b070f7f3a232b
Park-Beomjin <87348209+pbj97@users.noreply.github.com>
2022-06-22 14:05:49 +0900

    JS Home Contact Btn

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 'use strict';
 
+
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handle scrolling when trapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+    scrollIntoView(link);
+});
+
+
+// Handle click on "contact me" button on home
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click' , (event) => {
+    scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector) {
+    const scrollto = document.querySelector(selector);
+    scrollto.scrollIntoView({behavior: "smooth"});
+}
